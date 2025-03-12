652af13423816b078e63af8fcbd20829f7fdae26
Anderson <andersonlosada.11@gmail.com>
2025-03-12 17:53:36 -0500

    components

---
+++ src/components/assetForm.js
@@ -0,0 +1,15 @@
+// src/components/Mapa.js
+import React from "react";
+import styled from "styled-components";
+
+const MapaContainer = styled.div`
+  width: 100%;
+  height: 500px;
+  border: 1px solid #ccc;
+`;
+
+const Mapa = () => {
+  return <MapaContainer>Aquí va el mapa</MapaContainer>;
+};
+
+export default Mapa;
